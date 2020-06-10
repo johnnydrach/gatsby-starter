55c6f46bd1a0514140b9e810dce4a6129390b90a
Sam SmithJohn <sam@example.comjohn.drach@nutrien.com>
2020-06-10 07:41:49 -0600

    Added MDX content

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,15 +17,38 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images/`,
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `shit`,
-        path: `${__dirname}/src/posts/shit`,
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extensions: [".md", ".mdx"],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: ["roboto mono", "muli:400,400i,700,700i"],
+        display: "swap",
       },
     },
   ],
